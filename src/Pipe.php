91f009cec3327754bcd2fc1b53a5ca2b63c43feb
Petr Grishin <petr.grishin@grishini.ru>
2015-02-17 23:21:09 +0300

    Implementation class

---
+++ src/Pipe.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * @author Petr Grishin <petr.grishin@grishini.ru>
+ */
+
+namespace PetrGrishin\Pipe;
+
+
+use Closure;
+use ReflectionClass;
+
+class Pipe {
+
+    protected $pipes = [];
+    protected $passable;
+
+    public static function create($passable) {
+        return new static($passable);
+    }
+
+    public function __construct($passable){
+        $this->passable = $passable;
+    }
+
+    public function through($pipes) {
+        $this->pipes = is_array($pipes) ? $pipes : func_get_args();
+        return $this;
+    }
+
+    public function then(Closure $destination) {
+        $firstSlice = $this->getInitialSlice($destination);
+        $pipes = array_reverse($this->pipes);
+        return call_user_func(
+            array_reduce($pipes, $this->getSlice(), $firstSlice), $this->passable
+        );
+    }
+
+    /**
+     * Get a Closure that represents a slice of the application onion.
+     *
+     * @return Closure
+     */
+    protected function getSlice() {
+        return function ($stack, $pipe) {
+            return function ($passable) use ($stack, $pipe) {
+                if ($pipe instanceof Closure) {
+                    return call_user_func($pipe, $passable, $stack);
+                }
+                if (is_array($pipe)) {
+                    $class = $pipe[0];
+                    unset($pipe[0]);
+                    /** @var Closure $object */
+                    $object = (new ReflectionClass($class))->newInstanceArgs($pipe);
+                } else {
+                    /** @var Closure $object */
+                    $object = new $pipe();
+                }
+                return $object($passable, $stack);
+            };
+        };
+    }
+
+    /**
+     * Get the initial slice to begin the stack call.
+     *
+     * @param  Closure  $destination
+     * @return Closure
+     */
+    protected function getInitialSlice(Closure $destination) {
+        return function () use ($destination) {
+            return call_user_func($destination, $this->passable);
+        };
+    }
+}
---
+++ tests/unit/PipeTest.php
@@ -0,0 +1,66 @@
+<?php
+use PetrGrishin\Pipe\Pipe;
+
+/**
+ * @author Petr Grishin <petr.grishin@grishini.ru>
+ */
+
+class PipeTest extends PHPUnit_Framework_TestCase {
+    public function testPipe() {
+        $passable = 'test';
+        $i = 0;
+        PipeTest__Middleware::$run = function($_passable) use ($passable, &$i) {
+            $i = $i + 1;
+            $this->assertEquals($passable, $_passable);
+        };
+        $this->assertEquals(0, $i);
+        Pipe::create($passable)
+            ->through(['PipeTest__Middleware'])
+            ->then(function ($_passable) use ($passable, &$i) {
+                $this->assertEquals(1, $i);
+                $i = $i + 1;
+                $this->assertEquals($passable, $_passable);
+            });
+        $this->assertEquals(2, $i);
+    }
+
+    public function testPipeWithArguments() {
+        $passable = 'test';
+        $i = 0;
+        $argValue = 'argValue';
+        PipeTest__Middleware::$run = function($_passable, $arg1) use ($passable, $argValue, &$i) {
+            $i = $i + 1;
+            $this->assertEquals($passable, $_passable);
+            $this->assertEquals($argValue, $arg1);
+        };
+        $this->assertEquals(0, $i);
+        Pipe::create($passable)
+            ->through([['PipeTest__Middleware', $argValue]])
+            ->then(function ($_passable) use ($passable, &$i) {
+                $this->assertEquals(1, $i);
+                $i = $i + 1;
+                $this->assertEquals($passable, $_passable);
+            });
+        $this->assertEquals(2, $i);
+    }
+}
+
+class PipeTest__Middleware {
+    /** @var Closure */
+    public static $run;
+
+
+    public function __construct($arg1 = null) {
+        $this->arg1 = $arg1;
+    }
+
+    public function __invoke($passable, Closure $next) {
+        $run = self::$run;
+        if ($this->arg1) {
+            $run($passable, $this->arg1);
+        } else {
+            $run($passable);
+        }
+        return $next($passable);
+    }
+}
